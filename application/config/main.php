e59907bfef1a739ad21b26d2f973cb588e5a79ac
xles <xles@mirakulix.org>
2014-03-09 00:10:52 +0100

    Moved source out of the document root.

---
+++ application/config/main.php
@@ -0,0 +1,12 @@
+<?php
+
+// uncomment the following to define a path alias
+// Yii::setPathOfAlias('local','path/to/local-folder');
+
+// This is the main Web application configuration. Any writable
+// CWebApplication properties can be configured here.
+ 
+return array(
+    'basePath'    => dirname(__FILE__).'/../../application',
+    'runtimePath' => dirname(__FILE__).'/../../runtime'
+);

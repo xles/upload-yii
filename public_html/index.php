adfea18f8630a3753fa1d2540e1e1df1b5bff783
xles <xles@mirakulix.org>
2014-03-09 10:33:40 +0100

    Removed comments from index.php

--- public_html/index.php
+++ public_html/index.php
@@ -1,8 +1,6 @@
 <?php
 
-// include Yii bootstrap file
 require_once(dirname(__FILE__).'/../yii/framework/yii.php');
 $config = dirname(__FILE__) . '/../application/config/main.php';
 
-// create a Web application instance and run
 Yii::createWebApplication($config)->run();
